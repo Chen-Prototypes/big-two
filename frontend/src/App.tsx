5954f8a648f295d399074677dfdfb84c234532ea
Roger Chen <rogerchen803@gmail.com>
2024-03-29 15:47:20 -0600

    Create and style basic play page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Home from './pages/Home';
 
 const App = () => {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Notes />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</>
 	);
